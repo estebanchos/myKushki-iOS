71dfdc7c4737649ab4e8efc23f9cf0dcc443e44f
Carlos Ocampo <charlie@carlosocampo.ca>
2022-07-11 17:07:34 -0500

    Initial structure and styling of login page

--- myKushki/ContentView.swift
+++ myKushki/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Carlos O on 2022-07-11.
 //
-
+import Foundation
 import SwiftUI
 
 struct ContentView: View {
@@ -12,14 +12,59 @@ struct ContentView: View {
     @State var password = ""
     
     var body: some View {
-        VStack{
-            TextField("Username", text: $username)
-                .textContentType(.username)
+        VStack() {
+                   Text("Welcome to myKushki!")
+                       .font(.title)
+                       .foregroundColor(Color.white)
+                       .padding([.top, .bottom], 50)
+                       .shadow(radius: 6.0, x: 10, y: 10)
+                   
+                   VStack(alignment: .leading, spacing: 15) {
+                       TextField("Username", text: $username)
+                           .autocapitalization(.none)
+                           .disableAutocorrection(true)
+                           .padding()
+                           .background(Color(.white))
+                           .cornerRadius(25.0)
+                           .shadow(radius: 10.0, x: 5, y: 10)
+                       
+                       SecureField("Password", text: $password)
+                           .textContentType(.password)
+                           .padding()
+                           .background(Color(.white))
+                           .cornerRadius(25.0)
+                           .shadow(radius: 10.0, x: 5, y: 10)
+                   }
+                   .padding([.leading, .trailing], 50)
+                   Button(action: {} ) {
+                       Text("Forgot password?")
+                           .padding([.leading], 150)
+                           .foregroundColor(.white)
+                   }
+                   
+                   Button(action: { }) {
+                       Text("Sign In")
+                           .font(.headline)
+                           .foregroundColor(.white)
+                           .padding()
+                           .frame(width: 200, height: 60)
+                           .background(Color("Orange"))
+                           .cornerRadius(20.0)
+                           .shadow(radius: 10.0, x: 20, y: 10)
+                   }.padding(.top, 50)
+                   
+                   Spacer()
+                   HStack {
+                       Text("Don't have an account? ")
+                           .foregroundColor(.white)
+                       Button(action: {}) {
+                           Text("Sign Up")
+                           .foregroundColor(.yellow)
+                       }
+                   }
+               }
+        .background(Color("myKushki"))
             
-            TextField("Password", text: $password)
-                .textContentType(.password)
-        }
-        .padding([.leading, .trailing], 50)
     }
 }
 
