859334d50d31a661d13178a5b69bebffd25a5f4a
Carlos Ocampo <charlie@carlosocampo.ca>
2022-07-11 16:21:29 -0500

    Initial Commit

---
+++ myKushki/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myKushki
+//
+//  Created by Carlos O on 2022-07-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myKushki/myKushkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  myKushkiApp.swift
+//  myKushki
+//
+//  Created by Carlos O on 2022-07-11.
+//
+
+import SwiftUI
+
+@main
+struct myKushkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
