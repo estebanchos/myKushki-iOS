892d464333b575984b90c3b288b87ca45dc1327c
Carlos Ocampo <charlie@carlosocampo.ca>
2022-07-12 12:08:45 -0500

    feat: implemented webservice to authenticate user with server

---
+++ myKushki/Services/Webservice.swift
@@ -0,0 +1,63 @@
+//
+//  Webservice.swift
+//  myKushki
+//
+//  Created by Carlos O on 2022-07-12.
+//
+
+import Foundation
+
+enum AuthenticationError: Error {
+    case invalidCredentials
+    case custom(errorMessage: String)
+}
+
+struct LoginRequest: Codable {
+    let username: String
+    let password: String
+}
+
+struct LoginResponse: Codable {
+    let token: String?
+    let message: String?
+    let validLogin:Bool?
+}
+
+class Webservice {
+    
+    func login(username: String, password:String, completion: @escaping (Result<String, AuthenticationError>) -> Void) {
+        
+        guard let url = URL(string: "https://mykushki-serv.herokuapp.com/users/login") else {
+            completion(.failure(.custom(errorMessage: "URL is not correct")))
+            return
+        }
+        
+        let body = LoginRequest(username: username, password: password)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONEncoder().encode(body)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let data = data, error == nil else {
+                completion(.failure(.custom(errorMessage: "No data")))
+                return
+            }
+            
+            guard let loginResponse = try? JSONDecoder().decode(LoginResponse.self, from: data) else {
+                completion(.failure(.invalidCredentials))
+                return
+            }
+            
+            guard let token = loginResponse.token else {
+                completion(.failure(.invalidCredentials))
+                return
+            }
+            
+            completion(.success(token))
+            
+        }.resume()
+    }
+}
